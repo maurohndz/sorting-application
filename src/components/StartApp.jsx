cafff11e222ef7884c8a33cdfcfa9e6e7c917841
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-06-12 03:30:40 -0430

    done colors

---
+++ src/components/StartApp.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class StartApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default StartApp;
